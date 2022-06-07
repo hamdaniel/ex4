d4da3376620034e3da4bd14a9bfdc00ab53e14d7
Daniel Hamzany <daniel@hamzany.com>
2022-06-07 18:34:57 +0300

    moved files

--- Battle.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by user on 07/06/2022.
-//
-
-#include "Battle.h"
-
-Battle::Battle(CardName name, int force, int damage, int coins): Card(name), m_force(force), m_damage(damage), m_coins(coins) {};
-
--- Goblin.cpp
+++
@@ -1,8 +0,0 @@
-//
-// Created by user on 07/06/2022.
-//
-
-#include "Goblin.h"
-Vampire::Goblin(CardName name) : Battle(name,GOBLIN_FORCE,GOBLIN_DAMAGE,GOBLIN_COINS) {};
-
-
