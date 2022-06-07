de1ec7247c4e072f213a7fc88861209aa046317f
Daniel Hamzany <daniel@hamzany.com>
2022-06-07 16:37:15 +0300

    finished basic player

--- Player.cpp
+++ Player.cpp
@@ -2,3 +2,125 @@
 // Created by user on 07/06/2022.
 //
 
+#include "Player.h"
+#include "utilities.h"
+#include <string>
+
+const int MAX_LEVEL = 10;
+
+static string getJob();
+
+Player::Player(const char *name, int hp, int force) {
+    m_name = name;
+    if (force > 0) {
+        m_force = force;
+    } else {
+        m_force = DEFAULT_FORCE;
+    }
+    if (hp > 0) {
+        m_maxHp = hp;
+    } else {
+        m_maxHp = DEFAULT_HP;
+    }
+    m_coins = 0;
+    m_level = 1;
+    m_hp = m_maxHp;
+}
+
+void Player::levelUp()
+{
+    if(m_level<MAX_LEVEL){
+        m_level++;
+    }
+}
+
+int Player::getLevel() const
+{
+    return m_level;
+}
+
+void Player::buff(const int &forcePoints)
+{
+    if(forcePoints>0){
+        m_force+=forcePoints;
+    }
+}
+
+void Player::heal(const int &hpPoints)
+{
+    if(hpPoints>0){
+        if(m_hp+hpPoints<=m_maxHp){
+            m_hp+=hpPoints;
+        }
+        else{
+            m_hp=m_maxHp;
+        }
+    }
+}
+
+void Player::damage(const int &damagePoints)
+{
+    if(damagePoints>0){
+        if(m_hp-damagePoints>=0){
+            m_hp-=damagePoints;
+        }
+        else{
+            m_hp=0;
+        }
+    }
+}
+
+bool Player::isKnockedOut() const
+{
+    if(!m_hp){
+        return true;
+    }
+    else{
+        return false;
+    }
+}
+
+void Player::addCoins(const int &coins)
+{
+    if(coins>0){
+        m_coins+=coins;
+    }
+}
+
+bool Player::pay(const int &cost)
+{
+    if(m_coins<cost){
+        return false;
+    }
+    else
+    {
+        m_coins-=(cost>0)?cost:0;
+        return true;
+    }
+}
+
+int Player::getAttackStrength()
+{
+    return m_level+m_force;
+}
+
+std::ostream& operator<<(std::ostream& os, const Player& player)
+{
+    printPlayerDetails(os, player.m_name, player.getJob(), player.m_level, player.m_force, player.m_hp, player.m_coins);
+    return os;
+}
+
+/**
+ * returns the job of the player
+ * @return a string that contains the player's job
+ */
+static string getJob()
+{
+    if(dynamic_cast<Rogue>(this)!= nullptr){
+        return "Rogue";
+    }
+    if(dynamic_cast<Wizard>(this)!= nullptr){
+        return "Wizard";
+    }
+    return "Fighter";
+}
--- Player.h
+++ Player.h
@@ -5,4 +5,128 @@
 #ifndef EX4_PLAYER_H
 #define EX4_PLAYER_H
 
-#endif //EX4_PLAYER_H
+#include <string>
+#include "utilities.h"
+
+
+const int DEFAULT_FORCE = 5;
+const int DEFAULT_HP = 100;
+
+
+
+class Player {
+    /**
+     * concatenation operator for Player
+     * @return ostream with the Player data
+     */
+    friend std::ostream& operator<<(std::ostream& os, const Player& player)
+public:
+    /**
+     * C'tor of Player class
+     *
+     * @param name - The name of the player.
+     * @param force - The starting points of force of the player.
+     * @param hp - The max hp of the player.
+     *
+     */
+    Player(const char* name, int hp = DEFAULT_HP);
+
+    /**
+    * Here we are explicitly telling the compiler to use the default methods
+   */
+    Player(const Player&) = default;
+    ~Player() = default;
+    Player& operator=(const Player& other) = default;
+
+    /**
+     * increases the player's level by 1.
+     * if the level is already maxed out (level 10) - does nothing.
+     *
+     * @return
+     *      void
+     */
+    void levelUp();
+
+    /**
+     * return the player's current level.
+     *
+     * @return
+     *      int
+     */
+    int getLevel() const;
+
+    /**
+     * increases the player's force by the amount received as an argument.
+     * @param forcePoints - the amount of force points to be added.
+     *
+     * @return
+     *      void
+     */
+    void buff(const int& forcePoints);
+
+    /**
+    * increases the player's hp by the amount received as an argument.
+    * if hp is already maxed out - does nothing.
+    * @param hpPoints - the amount of hp points to be added.
+    *
+    * @return
+    *      void
+    */
+    void heal(const int& hpPoints);
+
+    /**
+    * lowers the player's hp by the amount received as an argument.
+    * @param damagePoints - the amount of hp points to be removed.
+    *
+    * @return
+    *      void
+    */
+    void damage(const int& damagePoints);
+
+    /**
+    * checks if hp has reached 0.
+    * prints "Player is knocked out" if hp is 0.
+     *
+    * @return
+    *      true - if hp=0.
+    *      false - else.
+    */
+    bool isKnockedOut() const;
+
+    /**
+    * increases the player's coins by the amount received as an argument.
+    *
+    * @param coins - the amount of coins to be added.
+    *
+    * @return
+    *      void
+    */
+    void addCoins(const int& coins);
+
+    /**
+    * removes the amount received as an argument from the player's coins - if the player has enough coins.
+    *
+    * @param cost - the amount of coins to be removed.
+    * @return
+    *      true - when coins were successfully removed.
+    *      false - when the player does not have enough coins (less than the amount received as an argument)
+    */
+    bool pay(const int& cost);
+
+    /**
+    * return the attack force (level+force).
+    *
+    * @return
+    *      int
+    */
+    int getAttackStrength();
+
+private:
+    std::string m_name;
+    int m_level;
+    int m_force;
+    int m_hp;
+    int m_coins;
+    int m_maxHp;
+};
+#endif //EX2_Player_H
