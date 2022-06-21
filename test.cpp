3ddd5d3c982658c52bd286e0df15d72ef04f1730
Daniel Hamzany <daniel@hamzany.com>
2022-06-21 16:43:53 +0300

    non bonus tests passed!!

--- test.cpp
+++ test.cpp
@@ -374,7 +374,7 @@ int main(){
 	run_test(badFormatStartTest,"Bad format at start of file exception test");
 	run_test(badFormatTest,"Bad format exception test");
 	run_test(noFileTest,"File Doesnt exist exception test");
-//	run_test(badSizeTest,"Bad size exception test");
+	run_test(badSizeTest,"Bad size exception test");
     run_test(roundLimitTest,"Round upper limit test");
     run_test(allTenTest,"All reach lvl 10 test");
     run_test(badPlayerInputTest,"Bad player input test");
